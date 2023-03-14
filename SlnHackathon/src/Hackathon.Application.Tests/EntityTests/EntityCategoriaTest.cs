dae26d2be9848c5c865378fb6bba957b8a5297b8
Tcharles da Maia <tcharlesmaia@yahoo.com.br>
2023-03-13 22:26:24 -0300

    Atualizacao com testes

---
+++ SlnHackathon/src/Hackathon.Application.Tests/EntityTests/EntityCategoriaTest.cs
@@ -0,0 +1,55 @@
+﻿using Hackathon.Domain.DTOs;
+using Hackathon.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon.Tests.EntityTests
+{
+    [TestFixture]
+    public class EntityCategoriaTest
+    {
+        [Test]
+        public void TestMapToEntity()
+        {
+            // Arrange
+            CategoriaDTO categoriaDTO = new CategoriaDTO
+            {
+                id = 1,
+                descricao = "Categoria Teste",
+                relatosList = new List<RelatoDTO>()
+            };
+
+            // Act
+            Categoria categoria = categoriaDTO.mapToEntity();
+
+            // Assert
+            Assert.AreEqual(1, categoria.Id);
+            Assert.AreEqual("Categoria Teste", categoria.Descricao);
+        }
+
+        [Test]
+        public void TestMapToDto()
+        {
+            // Arrange
+            Categoria categoria = new Categoria
+            {
+                Id = 1,
+                Descricao = "Categoria Teste",
+                RelatosList = new List<Relato>()
+            };
+
+            // Act
+            CategoriaDTO categoriaDTO = new CategoriaDTO().mapToDto(categoria);
+
+            // Assert
+            Assert.AreEqual(1, categoriaDTO.id);
+            Assert.AreEqual("Categoria Teste", categoriaDTO.descricao);
+        }
+
+
+    }
+}
+
---
+++ SlnHackathon/src/Hackathon.Application.Tests/EntityTests/EntityRelatoTest.cs
@@ -0,0 +1,95 @@
+﻿using Hackathon.Domain.DTOs;
+using Hackathon.Domain.Entities;
+using Hackathon.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon.Tests.EntityTests
+{
+    [TestFixture]
+    public class EntityRelatoTest
+    {
+        [Test]
+        public void TestMapToEntity()
+        {
+            // Arrange
+            RelatoDTO relatoDTO = new RelatoDTO
+            {
+                id = 1,
+                usuarioId = 1,
+                categoriaId = 2,
+                relatorio = "Relatório Teste",
+                rua = "Rua Teste",
+                bairro = "Bairro Teste",
+                cep = "12345-678",
+                cidade = "Cidade Teste", 
+                estado = "Estado Teste", 
+                imagem = "Imagem Teste",
+                data = DateTime.Now,
+                contadorLikes = 0,
+                status = StatusEnum.Novo
+            };
+
+            // Act
+            Relato relato = relatoDTO.mapToEntity();
+
+            // Assert
+            Assert.AreEqual(1, relato.Id);
+            Assert.AreEqual(1, relato.UsuarioId);
+            Assert.AreEqual(2, relato.CategoriaId);
+            Assert.AreEqual("Relatório Teste", relato.Relatorio);
+            Assert.AreEqual("Rua Teste", relato.Rua);
+            Assert.AreEqual("Bairro Teste", relato.Bairro);
+            Assert.AreEqual("12345-678", relato.CEP);
+            Assert.AreEqual("Cidade Teste", relato.Cidade);
+            Assert.AreEqual("Estado Teste", relato.Estado);
+            Assert.AreEqual("Imagem Teste", relato.Imagem);
+            Assert.AreEqual(relatoDTO.data, relato.Data);
+            Assert.AreEqual(0, relato.ContadorLikes);
+            Assert.AreEqual(StatusEnum.Novo, relato.Status);
+        }
+
+        [Test]
+        public void TestMapToDto()
+        {
+            // Arrange
+            Relato relato = new Relato
+            {
+                Id = 1,
+                UsuarioId = 1,
+                CategoriaId = 2,
+                Relatorio = "Relatório Teste",
+                Rua = "Rua Teste",
+                Bairro = "Bairro Teste",
+                CEP = "12345-678",
+                Cidade = "Cidade Teste",
+                Estado = "Estado Teste",
+                Imagem = "Imagem Teste",
+                Data = DateTime.Now,
+                ContadorLikes = 0,
+                Status = StatusEnum.Novo
+            };
+
+            // Act
+            RelatoDTO relatoDTO = new RelatoDTO().mapToDTO(relato);
+
+            // Assert
+            Assert.AreEqual(1, relatoDTO.id);
+            Assert.AreEqual(1, relatoDTO.usuarioId);
+            Assert.AreEqual(2, relatoDTO.categoriaId);
+            Assert.AreEqual("Relatório Teste", relatoDTO.relatorio);
+            Assert.AreEqual("Rua Teste", relatoDTO.rua);
+            Assert.AreEqual("Bairro Teste", relatoDTO.bairro);
+            Assert.AreEqual("12345-678", relatoDTO.cep);
+            Assert.AreEqual("Cidade Teste", relatoDTO.cidade);
+            Assert.AreEqual("Estado Teste", relatoDTO.estado);
+            Assert.AreEqual("Imagem Teste", relatoDTO.imagem);
+            Assert.AreEqual(0, relatoDTO.contadorLikes);
+            Assert.AreEqual(StatusEnum.Novo, relatoDTO.status);
+        }
+    }
+}
+
